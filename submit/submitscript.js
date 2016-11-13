e0a460ba01d5a024839e37bbf8f5884dff556b1e
JordanMcGhee <jtmc8b@mail.missouri.edu>
2016-11-13 16:56:36 -0600

    please java script

--- submit/submitscript.js
+++ submit/submitscript.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
-    $('nav ul').slideToggle(500);
+    $('.navcontainer').slideToggle(500);
   });
 
 $(window).resize(function() {
 	   if (  $(window).width() > 600 ) {
-		$('nav ul').removeAttr('style');
+		$('.navcontainer').removeAttr('style');
 	 }
 	});
 });
